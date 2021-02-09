ef2fa2bdccd640fd507165266af9667a841f4571
DaTaylorSeries <zippytato@gmail.com>
2021-02-08 19:16:56 -0700

    2/08/21: Application & Context Menu

--- JS/Primary/main.js
+++ JS/Primary/main.js
@@ -62,10 +62,102 @@ function createWindow() {
 app.whenReady().then(() => {
   createWindow()
 
+  const template = [
+    // File tab
+    {
+      label: 'File',
+      submenu: [
+        isMac ? { role: 'close' } : { role: 'quit' }
+      ]
+    },
+    // Edit tab
+    {
+      label: 'Edit',
+      submenu: [
+        { role: 'undo' },
+        { role: 'redo' },
+        { type: 'separator' },
+        { role: 'cut' },
+        { role: 'copy' },
+        { role: 'paste' },
+        { role: 'pasteandmatchstyle' },
+        { role: 'delete' },
+        { role: 'selectall' }
+      ]
+    },
+    // Help tab
+    {
+      label: 'Help',
+      submenu: [
+        {
+          label: 'About Electron',
+          click: function () {
+            electron.shell.openExternal('http://electron.atom.io');
+          },
+          accelerator: 'CmdOrCtrl + Shift + H'
+        }
+      ]
+    },
+    // Window tab
+    {
+      role: 'window',
+      submenu: [
+        { role: 'minimize' },
+        { role: 'close' }
+      ]
+    }
+  ];
+
+  // Build the application menu
+  const menu = Menu.buildFromTemplate(template);
+  Menu.setApplicationMenu(menu);
+
+  // Global shortcut to show the application
+  globalShortcut.register('Alt+1', function () {
+    win.show()
+  })
+
   app.on('activate', function () {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  });
+});
+
+// Create a context menu
+// This is the menu when you right click within an application
+app.on('browser-window-created', function (event, win) {
+  const ctxMenu = new Menu()
+
+  // Undo action
+  ctxMenu.append(new MenuItem({ role: 'undo' }))
+  win.webContents.on('context-menu', function (e, params) {
+    ctxMenu.popup(win, params.x, params.y)
+  });
+  // Redo action
+  ctxMenu.append(new MenuItem({ role: 'redo' }))
+  win.webContents.on('context-menu', function (e, params) {
+    ctxMenu.popup(win, params.x, params.y)
+  });
+  // Seperatory
+  ctxMenu.append(new MenuItem({ type: 'separator' }))
+  win.webContents.on('context-menu', function (e, params) {
+    ctxMenu.popup(win, params.x, params.y)
+  });
+  // Cut action
+  ctxMenu.append(new MenuItem({ role: 'cut' }))
+  win.webContents.on('context-menu', function (e, params) {
+    ctxMenu.popup(win, params.x, params.y)
+  });
+  // Copy action
+  ctxMenu.append(new MenuItem({ role: 'copy' }))
+  win.webContents.on('context-menu', function (e, params) {
+    ctxMenu.popup(win, params.x, params.y)
+  });
+  // Paste action
+  ctxMenu.append(new MenuItem({ role: 'paste' }))
+  win.webContents.on('context-menu', function (e, params) {
+    ctxMenu.popup(win, params.x, params.y)
   });
 });
 
